d374d5be3edbb2a63f973a41b835b53d43b5f511
Haille <hmlperkins@hotmail.com>
2021-09-24 12:54:53 +0200

    bobbing pastry movement
    idk if this is useful, just thought it might be cute

--- Assets/Scripts/PastryScript.cs
+++ Assets/Scripts/PastryScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PastryScript : MonoBehaviour
 {
+    public float startY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // make the pastry bounce up n down
+        Vector3 bob = new Vector3(transform.position.x, startY + 0.35f*Mathf.Sin(1.5f*Time.realtimeSinceStartup), 0);
+        //sin = Mathf.Sin(0.5f * Time.deltaTime);
+        transform.position = bob;
     }
 }
