07eeff50f4679b6b8ff6d6b7c194e49950be2e9b
Elsa Sjogren <sjogea19@wfu.edu>
2021-10-05 12:32:22 +0200

    Monster moving animations

--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -13,21 +13,53 @@ public class MonsterScript : CharacterInheritance
     public Sprite[] destroySprite = new Sprite[7];
 
     public float monsterSpeed;
+    float maxPos;
+    float minPos;
+
+    public float range;
+    private Vector3 startPos;
+    private int directionMonster = 1;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+        startPos = transform.position;
+        maxPos = startPos.x + range / 2;
+        minPos = startPos.x - range / 2;
     }
 
-    
-    void Update()
+    private void Update()
     {
-        
+        int count = 0;
+        Vector2 vel = rb2d.velocity;
+        vel.x = monsterSpeed * directionMonster;
+        rb2d.velocity = vel;
+        mySpriteRenderer.sprite = jumpSprite[count];
+        count++;
+        monsterDirection();
+        StartCoroutine(walkingAnimation(0.5f));  // animate
+    }
+
+    void monsterDirection()
+    {
+        //define a space within the monster can run and if he hits on edge turn and go the other way
+        if (transform.position.x > maxPos)
+        {
+            directionMonster = -1;
+            //turn left
+            mySpriteRenderer.flipX = true;
+        }
+        if (transform.position.x < minPos)
+        {
+            directionMonster = 1;
+            //turn right
+            mySpriteRenderer.flipX = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
-        //on collision with player should take away one of the players three/five lives 
+    //on collision with player should take away one of the players three/five lives 
     {
         if (collision.gameObject.CompareTag("Player"))
         {
@@ -40,5 +72,30 @@ public class MonsterScript : CharacterInheritance
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
         }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        maxPos = startPos.x + range / 2;
+        minPos = startPos.x - range / 2;
+        //called if you have selected the game object in editor
+        Vector3 minPosTransform = new Vector3(minPos, transform.position.y, -1);
+        Vector3 maxPosTransform = new Vector3(maxPos, transform.position.y, -1);
+        Debug.DrawLine(minPosTransform, maxPosTransform, Color.cyan);
+    }
+
+    // animate the walking with timePerFrame seconds between each frame
+    IEnumerator walkingAnimation(float timePerFrame)
+    {
+        for (int i = 0; i < jumpSprite.Length; i++)
+        {
+            yield return new WaitForSeconds(timePerFrame);
+            mySpriteRenderer.sprite = jumpSprite[i];
+            yield return new WaitForSeconds(timePerFrame);
+        }
+
+        // loop
+        StartCoroutine(walkingAnimation(timePerFrame));
     }
 }
